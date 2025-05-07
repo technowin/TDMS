e8e3fe221331cc0a98d0b92d0225a3e5b5dee6fc
technowin <technowinitinfra@gmail.com>
2025-05-07 11:22:27 +0530

    sfssf

--- Form/views.py
+++ Form/views.py
@@ -1057,7 +1057,7 @@ def common_form_post(request):
         handle_uploaded_files(request, form_name, created_by, form_data, user)
         handle_generative_fields(form, form_data, created_by)
 
-        callproc('create_dynamic_form_views')
+        # callproc('create_dynamic_form_views')
         messages.success(request, "Form data saved successfully!")
         if workflow_YN == '1':
             wfdetailsid = request.POST.get('wfdetailsid', '')
@@ -1212,7 +1212,7 @@ def common_form_edit(request):
 
         handle_uploaded_files(request, form_name, created_by, form_data, user)
 
-        callproc('create_dynamic_form_views')
+        # callproc('create_dynamic_form_views')
         messages.success(request, "Form data updated successfully!")
         if workflow_YN == '1E':
         
